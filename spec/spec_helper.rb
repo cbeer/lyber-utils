afacaa6639dabee6727e54118bd2f7f9ad4c1d79
Naomi Dushay <ndushay@stanford.edu>
2018-02-22 22:37:49 -0800

    spec_helper: remove cruft

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,4 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'coveralls'
 Coveralls.wear!
 
-# require 'rubygems'
-# require 'rspec'
-
-RSpec.configure do |config|
-
-end
-
 Rails = Object.new unless defined? Rails
